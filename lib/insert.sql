9e0a00cd2ed780704ee7f854c23e1dff5c14347a
Mera-Stackhouse <mera.stackhouse@gmail.com>
2019-03-21 17:11:18 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,26 @@
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("hello", 1, 2);
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("hello", 1, 2);
+
+INSERT INTO books (title, year, series_id) VALUES ("Hi", 2019, 2);
+INSERT INTO books (title, year, series_id) VALUES ("Hi", 2019, 2);
+INSERT INTO books (title, year, series_id) VALUES ("Hi", 2019, 2);
+INSERT INTO books (title, year, series_id) VALUES ("Hi", 2019, 2);
+INSERT INTO books (title, year, series_id) VALUES ("Hi", 2019, 2);
+INSERT INTO books (title, year, series_id) VALUES ("Hi", 2019, 2);
+
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ( "Sup", "human", "lets dance", 2, 1);
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ( "Sup", "human", "lets dance", 2, 1);
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ( "Sup", "human", "lets dance", 2, 1);
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ( "Sup", "human", "lets dance", 2, 1);
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ( "Sup", "human", "lets dance", 2, 1);
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ( "Sup", "human", "lets dance", 2, 1);
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ( "Sup", "human", "lets dance", 2, 1);
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ( "Sup", "human", "lets dance", 2, 1);
+
+INSERT INTO subgenres (name) VALUES ("scifi");
+INSERT INTO subgenres (name) VALUES ("scifi");
+
+INSERT INTO authors (name) VALUES ("bob");
+INSERT INTO authors (name) VALUES ("bob");
+
+INSERT INTO character_books (character_id, book_id) VALUES (1, 4), (3, 5), (3, 5), (3, 5), (3, 5), (3, 5), (3, 5), (3, 5), (3, 5), (3, 5), (3, 5), (3, 5), (3, 5), (3, 5), (3, 5), (3, 5);
